0c84701eaacd28c8bb3288ada227f74fa7d9fe19
Dave <davidgrace00@gmail.com>
2021-11-03 20:20:47 +0000

    Fixes for .NET 6 RC 2

--- RoundTheCode.BlazorDotNet6.Wasm/Program.cs
+++ RoundTheCode.BlazorDotNet6.Wasm/Program.cs
@@ -1,4 +1,7 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
